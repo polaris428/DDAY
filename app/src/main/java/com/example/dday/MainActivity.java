64c1dcd43762a7b52a7002db76d34ae07d1e71e5
polaris428 <61411085+polaris428@users.noreply.github.com>
2021-02-25 05:15:40 +0900

    커밋생성

---
+++ app/src/main/java/com/example/dday/MainActivity.java
@@ -0,0 +1,173 @@
+package com.example.dday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class MainActivity extends AppCompatActivity {
+    String TAG="polris";
+    int yy;
+    int hh;
+    int dd;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    CountDownTimer countDownTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        TextView text=(TextView)findViewById(R.id.text);
+
+        countDownTimer = new CountDownTimer(20000000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //tv_timer.setText(getTime());
+                text.setText(countdday(yy, hh,dd )+"일 "+getTime());
+                Log.d(TAG, "Value is: " + getTime());
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        countDownTimer.start();
+
+        DatabaseReference myRef = database.getReference("a");
+        DatabaseReference myRef2 = database.getReference("b");
+        DatabaseReference myRef3 = database.getReference("c");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Integer value = dataSnapshot.getValue(Integer.class);
+                yy= dataSnapshot.getValue(Integer.class);
+                Log.d(TAG, "Value is: " + value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Integer value = dataSnapshot.getValue(Integer.class);
+                hh= dataSnapshot.getValue(Integer.class);
+                Log.d(TAG, "Value is: " + value);
+                countdday(yy, hh,dd );
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        myRef3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Integer value = dataSnapshot.getValue(Integer.class);
+                dd= dataSnapshot.getValue(Integer.class);
+                Log.d(TAG, "Value is: " + value);
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+    }
+
+    public int countdday(int myear, int mmonth, int mday) {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd-hh");
+
+            Calendar todaCal = Calendar.getInstance(); //오늘날자 가져오기
+            Calendar ddayCal = Calendar.getInstance(); //오늘날자를 가져와 변경시킴
+
+            mmonth -= 1; // 받아온날자에서 -1을 해줘야함.
+            ddayCal.set(myear,mmonth,mday);// D-day의 날짜를 입력
+            Log.e("테스트",simpleDateFormat.format(todaCal.getTime()) + "");
+            Log.e("테스트",simpleDateFormat.format(ddayCal.getTime()) + "");
+
+            long today = todaCal.getTimeInMillis()/86400000; //->(24 * 60 * 60 * 1000) 24시간 60분 60초 * (ms초->초 변환 1000)
+            long dday = ddayCal.getTimeInMillis()/86400000;
+            long count = dday - today; // 오늘 날짜에서 dday 날짜를 빼주게 됩니다.
+            Log.d(TAG,"Value : "+count);
+            return (int) count;
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+    private String getTime(){
+        Date date = new Date();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int c_hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int c_min = calendar.get(Calendar.MINUTE);
+        int c_sec = calendar.get(Calendar.SECOND);
+
+        Calendar baseCal = new GregorianCalendar(year,month,day,c_hour,c_min,c_sec);
+        Calendar targetCal = new GregorianCalendar(year,month,day+2,0,0,0);  //비교대상날짜
+
+        long diffSec = (targetCal.getTimeInMillis() - baseCal.getTimeInMillis()) / 1000;
+        long diffDays = diffSec / (24*60*60);
+
+        targetCal.add(Calendar.DAY_OF_MONTH, (int)(-diffDays));
+
+        int hourTime = (int)Math.floor((double)(diffSec/3600));
+        int minTime = (int)Math.floor((double)(((diffSec - (3600 * hourTime)) / 60)));
+        int secTime = (int)Math.floor((double)(((diffSec - (3600 * hourTime)) - (60 * minTime))));
+
+        String hour = String.format("%02d", hourTime-24);
+        String min = String.format("%02d", minTime);
+        String sec = String.format("%02d", secTime);
+
+        return hour + " 시간 " +min + " 분 "+ sec + "초 남았습니다.";
+
+    }
+}
